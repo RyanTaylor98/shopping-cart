41285c600a693819ac12420332b82173e2ff0aa6
Ryan Taylor <ryantaylor98@Ryan.Taylor.98>
2018-02-21 17:54:06 +0000

    Initial commit

---
+++ src/DataInterface.java
@@ -0,0 +1,23 @@
+package shoppingbasket;
+
+import java.sql.*;  
+
+public class DataInterface {
+	public void readData(){  
+		try{  
+		Class.forName("com.mysql.jdbc.Driver");  
+		Connection con=DriverManager.getConnection(  
+		"jdbc:mysql://localhost:3306/assignment","ryan","test");  
+		//here sonoo is database name, root is username and password  
+		Statement stmt=con.createStatement();  
+		ResultSet rs=stmt.executeQuery("select * from books");  
+		while(rs.next())  
+		System.out.println(rs.getString(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+"  "+rs.getString(4));  
+		con.close();  
+		}catch(Exception e){ System.out.println(e);}  
+	}  
+	//TODO: connect to database
+	//TODO: read from database
+	//TODO: write to database
+
+}
